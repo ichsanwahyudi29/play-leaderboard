499170aad72a7c2fa06c5f0b367258d62725b5e0
Ichsan Wahyudi <ichsan.sano@gmail.com>
2018-12-04 20:06:09 +0700

    config for ext img, header unf, style

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,17 @@
 const path = require('path');
-const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+const isProduction = process.env.NODE_ENV === 'production';
 
 console.log('modee', process.env.NODE_ENV);
 
 const webpackConfig = {
   mode: process.env.NODE_ENV,
   devServer: {
-    // contentBase: './src',
     contentBase: './dist',
+    port: 3333,
   },
   devtool: 'source-map',
   entry: {
@@ -17,21 +19,49 @@ const webpackConfig = {
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: '[name].min.js',
+    filename: isProduction ? '[name].[contenthash].min.js' : '[name].js',
   },
   module: {
     rules: [
       {
         test: /\.scss$/,
         use: [
-          {
-            loader: 'style-loader',
-          },
+          MiniCssExtractPlugin.loader,
           {
             loader: 'css-loader',
           },
           {
             loader: 'sass-loader',
+          },
+        ],
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'assets/',
+              limit: 8192,
+            },
+          },
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              bypassOnDebug: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(html)$/,
+        use: [
+          {
+            loader: 'html-loader',
+            options: {
+              attrs: ['img:src'],
+            },
           },
         ],
       },
@@ -42,6 +72,10 @@ const webpackConfig = {
     new HtmlWebpackPlugin({
       template: './src/index.html',
       filename: 'index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: isProduction ? '[name].[contenthash].min.css' : '[name].css',
+      chunkFilename: '[id].css',
     }),
   ],
 };
