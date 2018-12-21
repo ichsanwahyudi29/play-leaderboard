3312fc1e60b48318e4a4033ee236cc048289a60d
Ariel Akilie <behtea@gmail.com>
2018-12-21 09:13:16 +0700

    integrate api

--- src/js/index.js
+++ src/js/index.js
@@ -1,43 +1,30 @@
-const dataRanking = [
-  {
-    ranking: '1',
-    name: 'Ichsan Indra Wahyudi',
-    skor: '19904',
-  },
-  {
-    ranking: '2',
-    name: 'Irwanto',
-    skor: '11604',
-  },
-  {
-    ranking: '3',
-    name: 'Rendi Christian',
-    skor: '19904',
-  },
-  {
-    ranking: '4',
-    name: 'Ariel Noah',
-    skor: '9904',
-  },
-  {
-    ranking: '5',
-    name: 'Ryan aja',
-    skor: '9904',
-  },
-  {
-    ranking: '6',
-    name: 'Mbo Dharmi',
-    skor: '9904',
-  },
-  {
-    ranking: '+9999',
-    name: 'Mpok ella',
-    skor: '9904',
-  },
-];
+const enviroment = 'production';
+var domain;
+var userId;
+
+if (enviroment === 'production') {
+  domain = 'https://chat.tokopedia.com';
+} else {
+  domain = 'https://chat-staging.tokopedia.com';
+}
+
+
+// fetch('https://gql.tokopedia.com', {
+//   method: 'POST',
+//   credentials: 'include',
+//   body: '{"query":"{\\n\\tprofile {\\n\\t\\tuser_id\\n\\t}\\n}"}',
+// }).then(response => response.json()).then((json) => {
+//   userId = json.profile.user_id;
+// }); 
+
+const endpointGetPrizeQuizWinner = '/gmf/api/v2/leaderboard/weekly_prize_quiz_winners';
+const endpointGetEvents = '/gcn/api/v2/leaderboard/get_events';
+const endpointGetLeaderRanking = '/gmf/api/v2/leaderboard/weekly_top_ten_winners';
 
 $(document).ready(function() {
   initDataRanking();
+  initDataPrizeQuizWinner();
+  initDataShowEvents();
 });
 
 function appendRankingElem(el) {
@@ -45,9 +32,148 @@ function appendRankingElem(el) {
   $('#js_ranking').append(el);
 }
 
-function initDataRanking() {
-  resultsRanking(dataRanking);
-  $('#js_ranking__count').text(dataRanking.length);
+function initDataRanking() {  
+  $.ajax({
+    url: domain + endpointGetLeaderRanking,
+    success: function (response) {
+      weekly_winners = response.data.weekly_winners
+      resultsRanking(weekly_winners);
+      $('#js_ranking__count').text(weekly_winners.length);
+    },
+    error: function () {
+      handleEmptyResult();
+    }    
+  });     
+}
+
+function initSearchDataRanking(winner_name) {
+  $.ajax({
+    url: domain + endpointGetLeaderRanking,
+    data: { winner_name: winner_name },
+    success: function (response) {
+      weekly_winners = response.data.weekly_winners
+      if (weekly_winners.length !== 0) {
+        resultsRanking(weekly_winners);
+      } else {
+        handleEmptyResult();
+      }
+    },
+    error: function () {
+      handleEmptyResult();
+    }
+  });
+}
+
+function initDataShowEvents() {
+  var settings = {
+    "crossDomain": true,
+    "method": "GET",
+    "headers": {
+      /* "Authorization": "bearer _fruNGSdSDKnhg7PDDoDCQ", */
+      "cache-control": "no-cache"
+    },
+    url: domain + endpointGetEvents,
+    success: function (response) {
+      data = response.data
+      resultNextEvent(data.next_event);
+      resultsAllEvents(data.all_events);
+      handleBtnReminder();
+    }    
+  }  
+  $.ajax(settings);    
+}
+
+function initDataPrizeQuizWinner() {
+  var settings = {
+    "crossDomain": true,
+    "url": domain + endpointGetPrizeQuizWinner,
+    "method": "GET",
+    "headers": {
+      "cache-control": "no-cache",
+    },
+    url: domain + endpointGetPrizeQuizWinner,
+    success: function (response) {      
+      resultsQuizWinner(response.data.weekly_prize_quiz_winner);
+    }    
+  }  
+  $.ajax(settings);  
+}
+
+function resultNextEvent(data) {
+
+  if (data === null ) {
+    return false
+  }  
+  
+  const reminderTrue = `<button class="unf-btn unf-btn--small quiz__program-reminder js_reminder-btn unf-btn--transparent">Hapus Pengingat</button>`;
+
+  const reminderFalse = `<button class="unf-btn unf-btn--small quiz__program-reminder js_reminder-btn">Ingatkan Saya</button>`;
+
+  const reminderButton = (data.is_reminded ? reminderTrue : reminderFalse);
+
+  const nextEvent = `
+    <h4 class="quiz__program-next">Jadwal Program NET. Play selanjutnya:</h4>
+    <h3 class="quiz__program-text">
+      ${data.name}
+    </h3>
+    <h3 class="quiz__program-clock">
+      ${data.start_time}
+    </h3>
+
+    <span data-event-id="${data.event_id}">
+      ${reminderButton}
+    </span>    
+
+    <a id="js_all-program" href="#leaderboard-program" class="quiz__program-all">Lihat jadwal selengkapnya</a>
+    
+  `;
+
+  $('#next-event').append(nextEvent);  
+}
+
+function resultsAllEvents(obj) {
+  const reminderFalse = `<button class="unf-btn unf-btn--small unf-btn--animate unf-btn--primary program__btn-reminder js_reminder-btn">Ingatkan Saya</button>`;
+
+  const reminderTrue = `<button class="unf-btn unf-btn--small unf-btn--animate program__btn-reminder js_reminder-btn unf-btn--secondary">Hapus Pengingat</button>`;
+  
+
+  for (const key of obj) {
+    const reminderButton = (key.is_reminded ? reminderTrue : reminderFalse);
+    const listAllEvents = `
+      <div class="row">
+        <div class="col-6">
+          <h6 class="program__name">
+            ${key.name}
+          </h6>
+          <span class="program__clock">
+            ${key.start_time}
+          </span>
+        </div>
+        <div class="col-6" data-event-id="${key.event_id}">
+          ${reminderButton}
+        </div>
+      </div>      
+    `;
+
+    $('#all-events-list-item').append(listAllEvents);
+  }     
+}
+
+function resultsQuizWinner(obj) {
+  for (const key of obj) {
+    const listPrizeQuizWinner = `
+      <div class="row">
+        <div class="col-6">
+          ${key.winner_name !== '' ? key.winner_name : 'Akan datang'}
+        </div>
+        <div class="col-6">
+          ${key.prize_name}
+        </div>
+      </div>      
+    `;
+
+    $('#quiz-winner-list-item').append(listPrizeQuizWinner);
+  }    
 }
 
 function resultsRanking(obj) {
@@ -66,20 +192,21 @@ function resultsRanking(obj) {
   $('#js_ranking__count').text('');
   $('#js_ranking-list-item').empty();
   for (const key of obj) {
+    const rank = key.rank > 9999 ? '+9999' : key.rank;
     const listRank = `
       <div class="row">
         <div class="col-9">
           <div class="ranking__item">
             <div class="ranking__item-num">
-              <span>${key.ranking}</span>
+              <span>${rank}</span>
             </div>
             <div class="ranking__item-name">
-              <span>${key.name}</span>
+              <span>${key.winner_name}</span>
             </div>
           </div>
         </div>
         <div class="col-3">
-          <span class="ranking__score">${key.skor}</span>
+          <span class="ranking__score">${key.score}</span>
         </div>
       </div>
     `;
@@ -168,35 +295,17 @@ $(function handleSearchRanking() {
     keypress: function(e) {
       const results = [];
       let val = $(this).val();
-      if (val) {
+      if (val) {        
         if (e.which == 13) {
-          $(this).blur()
-          for (let i = 0; i < dataRanking.length; i++) {
-            for (key in dataRanking[i]) {
-              if (
-                dataRanking[i][key].toLowerCase().indexOf(val.toLowerCase()) !=
-                -1
-              ) {
-                results.push(dataRanking[i]);
-              }
-            }
-          }
+          $(this).blur();
           handleLoaderResult();
-          setTimeout(() => {
-            if (results != 0) {
-              resultsRanking(results);
-            } else {
-              handleEmptyResult();
-            }
-          }, 3000);
+          initSearchDataRanking(val)
         }
       } else {
         if (e.which == 13) {
           $(this).blur()
           handleLoaderResult();
-          setTimeout(() => {
-            initDataRanking();
-          }, 3000);
+          initDataRanking();
         }
       }
     },
@@ -216,16 +325,18 @@ $(function handleResetSearchRanking() {
 
 // Reminder
 
-$(function handleBtnReminder() {
+function handleBtnReminder() {
   $('.js_reminder-btn--same').on({
     click: function(e) {
       let triggerBtn = $('.js_reminder-btn--same');
       for (let i = 0; i < triggerBtn.length; i++) {
         let _self = $(triggerBtn[i]);
+        let parent = _self.parent();
+        let eventId = $(parent).data("event-id");        
         if (_self.hasClass('quiz__program-reminder')) {
           _self.toggleClass('unf-btn--transparent');
           let condition = _self.hasClass('unf-btn--transparent');
-          onChangeBtnReminder(_self, condition);
+          onChangeBtnReminder(_self, condition, eventId);
         }
 
         if (_self.hasClass('program__btn-reminder')) {
@@ -233,7 +344,7 @@ $(function handleBtnReminder() {
             .toggleClass('unf-btn--primary')
             .toggleClass('unf-btn--secondary');
           let condition = _self.hasClass('unf-btn--secondary');
-          onChangeBtnReminder(_self, condition);
+          onChangeBtnReminder(_self, condition, eventId);
         }
       }
     },
@@ -242,20 +353,39 @@ $(function handleBtnReminder() {
   $('.js_reminder-btn').on({
     click: function() {
       let _self = $(this);
+      let parent = _self.parent();
+      let eventId = $(parent).data("event-id");
       _self.toggleClass('unf-btn--primary').toggleClass('unf-btn--secondary');
       let condition = _self.hasClass('unf-btn--secondary');
-      onChangeBtnReminder(_self, condition);
+      onChangeBtnReminder(_self, condition, eventId);
     },
   });
-});
+};
 
-function onChangeBtnReminder(el, bool) {
+function onChangeBtnReminder(el, bool, eventId) {
   if (bool) {
+    // put ajax request here
+    // var settings = {
+    //   "async": true,
+    //   "crossDomain": true,
+    //   "url": "https://chat-staging.tokopedia.com/gcn/api/v2/leaderboard/remind_me/5",
+    //   "method": "POST",
+    //   "headers": {
+    //     "Authorization": "Bearer _fruNGSdSDKnhg7PDDoDCQ",
+    //     "cache-control": "no-cache",
+    //     "Postman-Token": "479286b9-4d1e-416e-99c0-9172d5acfd9d"
+    //   }
+    // }
+
+    // $.ajax(settings).done(function (response) {
+    //   console.log(response);
+    // });    
     el.text('Hapus Pengingat');
     showToaster(
       'Pengingat berhasil terpasang. Notifikasi akan dikirim saat NET. Play dimulai.'
     );
   } else {
+    // put ajax request here
     el.text('Ingatkan Saya');
   }
 }
